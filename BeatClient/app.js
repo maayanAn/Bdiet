321bc0eff1cffc2a45e10678e4927ef246bd6864
ayelethol <ayelethol@walla.com>
2017-04-10 12:26:25 +0200

    added all the files

--- BeatClient/app.js
+++ BeatClient/app.js
@@ -1,5 +1,36 @@
-﻿var beatApp = angular.module('beatApp', []);
+﻿var beatApp = angular.module('beatApp', ['ngRoute']);
+
+beatApp.config(['$locationProvider', '$routeProvider',
+        function config($locationProvider, $routeProvider) {
+            $locationProvider.hashPrefix('!');
+
+            $routeProvider.
+                when('/personalZone', {
+                    template: 'html/personalZone.html',
+                    controller: 'personalZoneController'
+                }).
+                when('/menu', {
+                    template: 'html/menu.html',
+                    controller: 'menuController'
+                }).
+                when('/login', {
+                    template: 'html/login.html',
+                    controller: 'loginController'
+                }).
+                when('/register', {
+                    template: 'html/register.html',
+                    controller: 'loginController'
+                }).
+                when('/', {
+                    template: 'html/home.html',
+                    controller: 'homeController'
+                }).
+                otherwise('/');
+        }
+    ]);
 
 beatApp.controller('BeatController', function BeatController($scope) {
     $scope.name = 'hi Beat';
+
+
 });
