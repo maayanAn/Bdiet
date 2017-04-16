93dbaec078d76b50f3623beed093bf2a487a6475
ayelethol <ayelethol@walla.com>
2017-04-16 15:18:49 +0300

    menu.html + menuController - Danielle

--- BeatClient/controllers/menuController.js
+++ BeatClient/controllers/menuController.js
@@ -1,5 +1,11 @@
-var beatApp = angular.module('beatApp');
+﻿var beatApp = angular.module('beatApp');
 
 beatApp.controller('menuController', function menuController($scope) {
+
+    $scope.breakfast = 'פרוסת לחם, שתי כפות גבינה, ביצה קשה, עגבניה, מלפפון';
+    $scope.morningSnack = 'חטיף אנרגיה, פרי';
+    $scope.lunch = 'מנת חזה עוף, כוס פחמימה';
+    $scope.afternoonSnack = 'חטיף אנרגיה, פרי';
+    $scope.dinner = 'פרוסת לחם, שתי כפות קטג, עגבניה, מלפפון';
 
 });
