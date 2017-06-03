a23fc28b6b625eac6d9f0ee88c7027c0cc601c98
nofarbelo <nofarbelo@gmail.com>
2017-06-03 21:45:42 +0300

    l

--- BeatClient/controllers/loginController.js
+++ BeatClient/controllers/loginController.js
@@ -7,7 +7,7 @@ beatApp.controller('loginController', function loginController($scope, $rootScop
     $scope.login = function () {
         var req = {
             method: 'POST',
-            url: 'http://localhost:51404/api/Login', // 51149
+            url: 'http://localhost:51149/api/Login', // 51149
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin' : '*'
@@ -46,7 +46,7 @@ beatApp.controller('loginController', function loginController($scope, $rootScop
         else {
             var req = {
                 method: 'POST',
-                url: 'http://localhost:51404/api/Users',
+                url: 'http://localhost:51149/api/Users',
                 headers: {
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Origin': '*'
